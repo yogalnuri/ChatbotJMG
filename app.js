0ce8e1d2dc261a5217d409e71948261ec5b10116
ACERPORTATIL\yogal <yogalnuri@gmail.com>
2018-05-15 22:02:54 +0200

    no message

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/update', function(req, res) {
+/*app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
@@ -75,8 +75,8 @@ app.post('/update', function(req, res) {
             }
         );
     });
-});
-app.get('/update', function(req, res) {
+});*/
+/*app.get('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
@@ -106,7 +106,8 @@ app.get('/update', function(req, res) {
             }
         );
     });
-});
+});*/
+app.post('/chatConnect', function(req,res))
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
